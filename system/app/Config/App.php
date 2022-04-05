96a34045711d8202a0123e1635adbf57d1b2d939
Indra Dio Aji Prayoga <indrada.prayoga@gmail.com>
2022-04-05 15:04:40 +0700

    update

--- system/app/Config/App.php
+++ system/app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://winteq-astra.com/cash/app';
+    public $baseURL = 'http://localhost/cash/app';
 
     /**
      * --------------------------------------------------------------------------
